ed141f39dbb34269478aa0d8c334d8e8b7a948b7
cosmobiosis <cosmobiosis@gmail.com>
2022-01-30 17:22:02 -0800

    fix: init

--- azure/HttpTrigger1/__init__.py
+++ azure/HttpTrigger1/__init__.py
@@ -1,24 +1,22 @@
+from email import header
 import logging
+import json
 from . import deepeye
 import azure.functions as func
-
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     eyetracker = deepeye.DeepEye()
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    req_body = req.get_json()
+    ret_json = {}
+    ret_code = 200
+    try:
+        ret_json = eyetracker.processSingleImage(req_body["image_base64"])
+    except Exception as e:
+        ret_json = { "ERR" : str(e) }
+        ret_code = 500
+    return func.HttpResponse(
+        body=json.dumps(ret_json),
+        status_code=ret_code,
+        headers={'content-type':'application/json'}
+    )
--- azure/HttpTrigger1/deepeye.py
+++ azure/HttpTrigger1/deepeye.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import pathlib
+import base64
 
 class Network:
     def __init__(self, name, imgs, batch_size, is_training=True, reuse=None, layers=16, deep=2):
@@ -149,7 +150,6 @@ class Network:
             
             return x
 
-
 class DeepEye:
     def __init__(self, deep=2, layers=16, model='/models/default.ckpt'):
 
@@ -238,3 +238,72 @@ class DeepEye:
         tf.reset_default_graph()
         # plt.close('all')
         self.sess.close()
+
+    def processSingleImage(self, image_encoded_data):
+        img_data = base64.b64decode(image_encoded_data)
+        npimg = np.frombuffer(img_data, dtype=np.uint8)
+        frame = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
+
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # PUPIL!!!
+        (coords, bw_img) = self.run(frame_gray)
+        contours, _ = cv2.findContours(bw_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_poly = [None] * len(contours)
+        boundRect = [None] * len(contours)
+        centers = [[]] * len(contours)
+        pupilRadiuses = [0] * len(contours)
+
+        for i, c in enumerate(contours):
+            contours_poly[i] = cv2.approxPolyDP(c, 3, True)
+            boundRect[i] = cv2.boundingRect(contours_poly[i])
+            centers[i], pupilRadiuses[i] = cv2.minEnclosingCircle(contours_poly[i])
+
+        pupilX = pupilY = pupilR = float('-inf')
+        for i in range(len(contours)):
+            if 2 * pupilRadiuses[i] > bw_img.shape[1] / 2:
+                # filter big diameter
+                continue
+            if pupilRadiuses[i] > pupilR:
+                pupilX = int(centers[i][0])
+                pupilY = int(centers[i][1])
+                pupilR = int(pupilRadiuses[i])
+        pupilX = -1 if pupilX == float('-inf') else pupilX
+        pupilY = -1 if pupilY == float('-inf') else pupilY
+        pupilR = -1 if pupilR == float('-inf') else pupilR
+
+        # IRIS!!!!!
+        irisCannyThreshold = 60
+        irisAccumulatorThreshold = 40
+        irisRadMin = 3 * int(frame_gray.shape[1] / 40)
+        irisRadMax = 10 * int(frame_gray.shape[1] / 40)
+        irisCircles = cv2.HoughCircles(cv2.cvtColor(cv2.medianBlur(frame,5), cv2.COLOR_BGR2GRAY), cv2.HOUGH_GRADIENT, 1, 1,
+                                  param1=irisCannyThreshold, param2=irisAccumulatorThreshold, minRadius=irisRadMin, maxRadius=irisRadMax)
+        irisCircles = np.uint16(np.around(irisCircles))
+
+        irisX = irisY = irisR = -1
+        for candidate in irisCircles[0, :]:
+            tempIrisX, tempIrisY, tempIrisR = candidate[0], candidate[1],candidate[2]
+            curDist = np.sqrt(np.sum(np.square(np.array([tempIrisX, tempIrisY]) - np.array([pupilX, pupilY]))))
+            if curDist + pupilR >= tempIrisR:
+                # wrong location for iris
+                continue
+            irisX, irisY, irisR = tempIrisX, tempIrisY, tempIrisR
+            break
+
+        height = frame_gray.shape[0]
+        width = frame_gray.shape[1]
+        return {
+            "pupilIrisRatio" : float(pupilR) / float(irisR),
+            "imgRatio": float(height) / float(width),
+            "pupil": {
+                "r" : float(pupilR) / float(width),
+                "x" : float(pupilX) / float(width),
+                "y": float(pupilY) / float(height),
+            },
+            "iris": {
+                "r": float(irisR) / float(width),
+                "x": float(irisX) / float(width),
+                "y": float(irisY) / float(height),
+            }
+        }
